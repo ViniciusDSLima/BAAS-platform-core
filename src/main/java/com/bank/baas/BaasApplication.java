4e512fdc1898c82b370842f765667fa2993b0e46
ViniciusDSLima <vinicius.devjvm@gmail.com>
2025-07-13 23:50:49 -0300

    refactor: update BaasApplication main class
    - Refactor main application class for BAAS platform
    - Update package structure and configuration
    - Improve Spring Boot application initialization

--- src/main/java/com/bank/baas/BaasApplication.java
+++ src/main/java/com/bank/baas/BaasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthorizerApplication {
+public class BaasApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizerApplication.class, args);
+        SpringApplication.run(BaasApplication.class, args);
     }
 
 }
